337b6c4218e9b7f1b7a495b66efc7175ad7a19f7
Dan Le <dqle202003@gmail.com>
2025-10-21 18:31:16 -0500

    Fixed LUI command

--- decode.v
+++ decode.v
@@ -51,7 +51,6 @@ module decode (
             end
             `OPCODE_LUI: begin // cheating for this one
                 alu_op = `ALU_ADDI;
-                opcode = `OPCODE_R_TYPE;
                 rs1 = 5'b00000;
             end
         endcase
@@ -78,10 +77,9 @@ module decode (
             end
             default: writeback = 1'b0;
         endcase
-
+        // Write Enable
         case (opcode)
-            // Add other opcodes here that need we
-            `OPCODE_I_TYPE, `OPCODE_LUI: begin
+            `OPCODE_R_TYPE, `OPCODE_I_TYPE, `OPCODE_LUI: begin
                 we = 1'b1;
             end
             default: we = 1'b0;
