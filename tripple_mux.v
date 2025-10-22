41d66604816b779f2ce2d9d7ad0f26d730805454
Elizabeth Bastow <ebastow@luc.edu>
2025-10-21 22:35:17 -0500

    jalr broke some stuff but my computer is gonna die so i cant test any more tonight

---
+++ tripple_mux.v
@@ -0,0 +1,24 @@
+`include "define.vh"
+
+module tripple_mux(plus_4, jump, ra, sel, out);
+input [31:0] plus_4,jump, ra;
+input [6:0] sel;
+output reg [31:0] out;
+
+always @ (*) begin
+
+    case (sel)
+        `OPCODE_I_TYPE, `OPCODE_JAL: begin
+            out = jump;
+        end
+	`OPCODE_I_TYPE, `OPCODE_JALR: begin
+            out = ra;
+        end
+        default: begin
+            out = plus_4;
+        end
+    endcase
+
+end
+
+endmodule
