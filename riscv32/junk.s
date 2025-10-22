80227ddc05a364582c6e9bc7638531a945674c1a
Elizabeth Bastow <ebastow@luc.edu>
2025-10-22 13:26:16 -0500

    adding tripple mux back in

--- riscv32/junk.s
+++ riscv32/junk.s
@@ -1,14 +1,16 @@
 _start:
+        addi x2, x0, 0xff
 	jal x1, 0x20
-	addi x2, x0, 0xff
-	addi x2, x1, 1
-	addi x3, x1, -1
-	sub x4, x3, x2
 	and x5, x5, x4
-	or x6, x2, x5
-	lui x7, 0x11100 # equals 0x100000
-	addi x7, x7, 0x0000
-	addi x8, x7, 0xf # should equal 0x10000f
-	lui x9, 0x00001
-	jalr x1, x1, 0
+        or x6, x2, x5
+        lui x7, 0x11100 # equals 0x100000
+        addi x7, x7, 0x0000
+        addi x8, x7, 0xf # should equal 0x10000f
+        lui x9, 0x00001
+	addi x2, x0, 0xff
+	addi x2, x0, 0x1
+	addi x2, x0, 0x2
+	addi x2, x0, 0x3
+	addi x2, x0, 0x4
+	
 	add x10, x7, x9
